6e62b9b4b3edcb2eb3e9d7542dc07eb9d3f88721
mahmoudadel <mahmoud.adel@koinz.app>
2022-08-13 18:06:14 +0200

    #0 - Add Explore Projects With Home Wrapper

--- src/components/HomeWrapper/index.tsx
+++ src/components/HomeWrapper/index.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { useDB } from "../../hooks/useDB";
+import { ExploreProjects } from "../ExploreProjects";
 import { Title } from "../Title";
 
 export const HomeWrapper: React.FC = () => {
   const db = useDB();
   return (
-    <div>
-      <Title 
-        title="Hello From Title CP"
-      />
-    </div>
+    <>
+      <ExploreProjects />
+    </>
   )
 }
