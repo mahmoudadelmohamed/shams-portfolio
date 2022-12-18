2e2a5070555db140726a85988bfd3ec3f9f26d39
Mahmoud Adel <mahmoud.adel@koinz.app>
2022-12-18 22:07:05 +0200

    #0 - Fix Categories Screen & Add projectDetails Screen

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,10 +1,12 @@
+import { Categories } from './src/context/types';
 import type { GatsbyNode } from "gatsby"
 import { initFirebaseApp, getDBOnce } from './src/utilis/firebase';
 import path from 'path';
+import type { DBContextProps } from './src/context/DBContext';
 
 export const createPages: GatsbyNode['createPages'] = async function ({ actions }) {
   initFirebaseApp();
-  const dbValue = await getDBOnce();
+  const dbValue: DBContextProps = await getDBOnce();
   actions.createPage({
     path: 'about',
     component: path.resolve('./src/templates/about.tsx'),
@@ -13,19 +15,32 @@ export const createPages: GatsbyNode['createPages'] = async function ({ actions 
   actions.createPage({
     path: '/',
     component: path.resolve('./src/templates/index.tsx'),
-    context: { dbValue },
+    context: {
+      homeCards: dbValue.categories.map((category) => ({
+        title: category.title,
+        image: dbValue.images.find((image) => image.id === category.id)?.image,
+        imageId: category.id,
+      }))
+    },
   });
   actions.createPage({
     path: '404',
     component: path.resolve('./src/templates/404.tsx'),
     context: { dbValue },
   });
-
-  dbValue.categories.forEach((category: any) => {
+  actions.createPage({
+    path: 'projectDetails',
+    component: path.resolve('./src/templates/projectDetails.tsx'),
+    context: { dbValue },
+  });
+  dbValue.categories.forEach((category) => {
     actions.createPage({
       path: `/categories/${category.title}`,
       component: path.resolve('./src/templates/category.tsx'),
-      context: { category, dbValue },
+      context: {
+        categoryTitle: category.title,
+        projects: dbValue.projects,
+      },
     });
   });
 }
