ee65d14634aeafad01b4d22c270aae5f0ce45f12
Mahmoud Adel <mahmoud.adel@koinz.app>
2022-12-23 19:20:27 +0200

    #0 - Refactor useImages Hook & Add Logo

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,7 +1,9 @@
 import star from '../images/star.png';
 import placeholder from '../images/placeholder.jpg';
+import logo from '../images/logo.png';
 const images = {
   star,
   placeholder,
+  logo,
 }
 export default images;
--- src/hooks/useImages/index.ts
+++ src/hooks/useImages/index.ts
@@ -1,16 +1,6 @@
-import { CompareArray } from './types';
-import { DBContextProps } from "../../context/DBContext";
 import { Image } from '../../context/types';
 
 export const useImages = (images: Image[], projectImages: number[]) => {
-  let counter = 0, arr: string[] = [];
-  let reduceImage = images.reduce((acc, curr) => {
-    if (projectImages[counter] === curr.id) {
-      acc.push(curr.image);
-      counter++;
-    }
-    return acc;
-  }, arr);
- 
-  return reduceImage;
+  let findImages = projectImages.map((projectId) => images.find((image) => image.id === projectId));
+  return findImages;
 }
